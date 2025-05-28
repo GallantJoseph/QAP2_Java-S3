35d38a3f5c290f1deb3957964c15e4ef22793800
GallantJoseph <gallant.joseph@outlook.com>
2025-05-28 11:58:43 -0230

    Person Class
    Completion of the Person class

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,25 @@
+/*
+    Description: QAP 2 - Problem 3 - Person Class
+    Author: Joseph Gallant
+    Date: May 28, 2025
+ */
+
 public class Person {
+    // Private attributes
+    private String lastName;
+    private String firstName;
+    private Address home;
+
+    // Constructors
+    // Parameterized
+    public Person(String firstName, String lastName, Address home) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.home = home;
+    }
+
+    // toString method overload
+    public String toString() {
+        return (firstName + " " + lastName + ", " + home.toString());
+    }
 }
