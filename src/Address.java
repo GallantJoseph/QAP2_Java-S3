bf0d96b7f2f335cab715b37fefbf2dc33fb53465
GallantJoseph <gallant.joseph@outlook.com>
2025-05-28 11:53:12 -0230

    Address Class
    -Completion of the Address class

--- src/Address.java
+++ src/Address.java
@@ -1,2 +1,27 @@
+/*
+    Description: QAP 2 - Problem 3 - Address Class
+    Author: Joseph Gallant
+    Date: May 28, 2025
+ */
+
 public class Address {
+    // Private attributes
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+
+    // Constructors
+    // Parameterized
+    public Address(String street, String city, String state, String zip) {
+        this.street = street;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    // toString method overload
+    public String toString() {
+        return (street + ", " + city + ", " + state + ", " + zip);
+    }
 }
