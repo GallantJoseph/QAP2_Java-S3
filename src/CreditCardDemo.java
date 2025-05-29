d9a53ae33bf57ab0b97eecdfa87086d288398af3
GallantJoseph <gallant.joseph@outlook.com>
2025-05-29 12:39:20 -0230

    CreditCardDemo - removed old tests

--- src/CreditCardDemo.java
+++ src/CreditCardDemo.java
@@ -7,33 +7,6 @@
 // Task 2.7: Compile, debug, and test the Address, Person, Money, and CreditCard classes
 public class CreditCardDemo {
     public static void main(String[] args) {
-//        Money m1 = new Money(-3.23);
-//        Money m2 = new Money(0.72);
-//        Money m3 = m1.add(m2);
-//        Money m4 = new Money(-3.23);
-//
-//        // -3.23 + 0.72 = -2.51
-//        System.out.println(m1);
-//
-//        m1.subtract(m2);
-//
-//        //-2.51 - 0.72 = -3.23
-//        System.out.println(m1);
-//
-//        System.out.println(m1.compareTo(m2));
-//        System.out.println(m2.compareTo(m1));
-//        System.out.println(m2.compareTo(m2));
-//
-//        System.out.println(m1.equals(m2));
-//        System.out.println(m1.equals(m4));
-//
-//        Address addr1 = new Address("123 Main", "St. John's", "NL", "A0A0A0");
-//        Money creditLimit = new Money(600);
-//
-//        CreditCard cc1 = new CreditCard(new Person("John", "Doe", addr1), creditLimit);
-//
-//        System.out.println(cc1.getPersonals());
-
         final Money LIMIT = new Money(1000);
         final Money FIRST_AMOUNT = new Money(200);
         final Money SECOND_AMOUNT = new Money(10.02);
@@ -48,19 +21,19 @@ public class CreditCardDemo {
         CreditCard visa = new CreditCard(owner, LIMIT);
         System.out.println(visa.getPersonals());
         System.out.println("Balance: " + visa.getBalance());
-        System.out.println("Credit Limit : " + visa.getCreditLimit());
+        System.out.println("Credit Limit: " + visa.getCreditLimit());
         System.out.println();
         System.out.println("Attempt to charge " + FIRST_AMOUNT);
         visa.charge(FIRST_AMOUNT);
-        System.out.println("Balance : " + visa.getBalance());
+        System.out.println("Balance: " + visa.getBalance());
         System.out.println("Attempt to charge " + SECOND_AMOUNT);
         visa.charge(SECOND_AMOUNT);
-        System.out.println("Balance : " + visa.getBalance());
+        System.out.println("Balance: " + visa.getBalance());
         System.out.println("Attempt to pay " + THIRD_AMOUNT);
         visa.payment(THIRD_AMOUNT);
-        System.out.println("Balance : " + visa.getBalance());
+        System.out.println("Balance: " + visa.getBalance());
         System.out.println("Attempt to charge " + FOURTH_AMOUNT);
         visa.charge(FOURTH_AMOUNT);
-        System.out.println("Balance : " + visa.getBalance());
+        System.out.println("Balance: " + visa.getBalance());
     }
 }
